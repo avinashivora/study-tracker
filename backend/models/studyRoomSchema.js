d39d1b60f3b43bc7566abbb04a94ed90389e2c08
Mokshj204 <158202022+Mokshj204@users.noreply.github.com>
2024-08-24 20:05:30 +0530

    added packages and folder structure

---
+++ backend/models/studyRoomSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const studyRoomSchema = new mongoose.Schema({
+  roomType: {
+    type: String,
+    required: true,
+    default : "private"
+  },
+  roomName : {
+    type : String,
+    required : false
+  },
+  roomCode : {
+    type: String,
+    required : true,
+    unique : true
+  },
+  createdOn : {
+    type : new Date,
+    required : true
+  },
+  roomOwner : {
+    ref : 'User',
+    required : true
+  }
+});
+
+module.exports = mongoose.model("study_room", studyRoomSchema);
